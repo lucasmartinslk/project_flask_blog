527ec2f82ad09c9f3a5bb93f81d7656ef54b06a3
Lucas Martins <lucas1993x@hotmail.com>
2020-08-14 17:22:00 -0300

    Login concluído com comunicacao do servidor

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,13 @@
-from app import app
-from flask import render_template
+from app import app, db, lm
+from flask_login import login_user
+from flask import render_template, flash
 from app.models.forms import LoginForm
+from app.models.tables import User
+
+
+@lm.user_loader
+def load_user(id):
+  return User.query.filter_by(id=id).first()
 
 
 @app.route("/")
@@ -13,8 +20,20 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("Logado!")
+        else:
+            flash("Login Inválido")
     else:
         print(form.errors)
     return render_template('login.html', form=form)
+
+
+@app.route("/teste/<info>")
+@app.route("/teste", defaults={"info": None})
+def teste(info):
+    r = User.query.filter_by(username='lukita').all()
+    print(r)
+    return "OK"
--- app/models/tables.py
+++ app/models/tables.py
@@ -10,6 +10,22 @@ class User(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+    
+
     def __init__(self, username, password, name, email):
         self.username = username
         self.password = password
