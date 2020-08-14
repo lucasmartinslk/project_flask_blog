244b62c202d2ba0b84de7c8aaefb5e380ff112db
Lucas Martins <lucas1993x@hotmail.com>
2020-08-14 15:25:46 -0300

    Conclusão dos htmls de login e index, confirmacão de validade de formulário e b loginform

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
+from app.models.forms import LoginForm
 
 
 @app.route("/")
+@app.route("/index/")
 def index():
-    return "Oie"
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form)
